9a9f853c770854d92d71740017950464313367c4
KaysonWhitehouse <109711622+KaysonWhitehouse@users.noreply.github.com>
2025-09-15 10:19:50 +1000

    Update PlayerMove.cs
    Added speed to player

--- COMP2160 Live Lecture Malcolm/Assets/Scripts/PlayerMove.cs
+++ COMP2160 Live Lecture Malcolm/Assets/Scripts/PlayerMove.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private Vector3 move;
+    [SerializeField] private Vector3 move = Vector3.up;
+    [SerializeField] private float speed = 5.0f;
+
+    void Awake()
+    {
+
+    }
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        transform.Translate(move * Time.deltaTime);        
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
