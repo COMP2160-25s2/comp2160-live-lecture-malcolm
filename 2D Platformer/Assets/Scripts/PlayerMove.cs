3987d2eec3f7dea706d03fe701ab8436605f3205
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2025-10-06 17:31:03 +1100

    Added horizontal movement

--- 2D Platformer/Assets/Scripts/PlayerMove.cs
+++ 2D Platformer/Assets/Scripts/PlayerMove.cs
@@ -7,8 +7,9 @@
  * For Unity Version: 6000.0.53f1
  */
 
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
@@ -17,6 +18,7 @@ public class PlayerMove : MonoBehaviour
 #region Parameters
     [SerializeField] private float gravity = -10f;  // m/s/s
     [SerializeField] private float maxFallSpeed = -20f; // m/s 
+    [SerializeField] private float horizontalSpeed = 10f;  // m/s/s
 #endregion 
 
 #region Connected Objects
@@ -27,6 +29,8 @@ public class PlayerMove : MonoBehaviour
 #endregion
 
 #region State
+    private Actions actions;
+    private InputAction moveAction;
 #endregion
 
 #region Properties
@@ -39,6 +43,18 @@ public class PlayerMove : MonoBehaviour
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        actions = new Actions();
+        moveAction = actions.PlayerMove.Move;
+    }
+
+    void OnEnable() 
+    {
+        actions.PlayerMove.Enable();
+    }
+
+    void OnDisable() 
+    {
+        actions.PlayerMove.Disable();
     }
 #endregion 
 
@@ -48,12 +64,25 @@ public class PlayerMove : MonoBehaviour
     }
 #endregion
 
+// Time     Update      FixedUpdate     pos (v=10m/s)
+// 0.0      1           1               0
+// 0.03     1           1               0.2m
+// 0.06     1           2               0.6m
+// 0.09     1           1               0.8m
+// 0.12     1           2               1.2m
+
 #region FixedUpdate
     void FixedUpdate()
     {
         Vector3 v = rigidbody.linearVelocity;
+
+        // vertical movement
         v.y += gravity * Time.fixedDeltaTime;
         v.y = Mathf.Max(v.y, maxFallSpeed);     // clamp to maxFallSpeed
+
+        // horizontal movement
+        v.x = moveAction.ReadValue<float>() * horizontalSpeed;
+
         rigidbody.linearVelocity = v;
     }
 #endregion
@@ -61,7 +90,7 @@ public class PlayerMove : MonoBehaviour
 #region Gizmos
     void OnDrawGizmos()
     {
-        if (!Application.isPlaying)
+        if (Application.isPlaying)
         {
             Handles.color = Color.white;
             Handles.Label(transform.position, $"rigidbody.linearVelocity = {rigidbody.linearVelocity}"); 
